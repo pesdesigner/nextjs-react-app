e8d5c476b5672c064cce4c3e417ceb4433cbe78d
pesdesigner <pauloeduardo.dg@outlook.com>
2021-09-12 20:27:01 -0300

    Next JS e React App

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className={`
+      flex h-screen justify-center items-center
+      bg-gradient-to-r from-purple-500 via-purple-300
+    `}>
+      <span>Texto</span>
+    </div>
+  )
+}
