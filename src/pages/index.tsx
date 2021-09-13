8a2ad5caa619f138c386cdfc6c812c4d3d87d8ab
pesdesigner <pauloeduardo.dg@outlook.com>
2021-09-12 21:53:26 -0300

    Componentes Layout e Título

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
     <div className={`
       flex h-screen justify-center items-center
-      bg-gradient-to-r from-purple-500 via-purple-300
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
     `}>
-      <span>Texto</span>
+      <Layout titulo="Cadastro simples">
+          <span>Conteúdo</span>
+      </Layout>
     </div>
   )
 }
