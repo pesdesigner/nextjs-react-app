46fc61a7f237f7370fe79225f92dca06a48f2651
pesdesigner <pauloeduardo.dg@outlook.com>
2021-09-15 23:01:32 -0300

    Componente Tabela

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,24 @@
 import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 54, '1'),
+    new Cliente('Bia', 44, '2'),
+    new Cliente('Carlos', 33, '3'),
+    new Cliente('Pedro', 28, '4')
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(`Excluir: ${cliente.nome}`)
+  }
+
   return (
     <div className={`
       flex h-screen justify-center items-center
@@ -8,7 +26,10 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro simples">
-          <span>Conteúdo</span>
+          <Tabela clientes={clientes}
+            clienteSelecionado={clienteSelecionado}
+            clienteExcluido={clienteExcluido}
+          />
       </Layout>
     </div>
   )
